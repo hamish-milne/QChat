6da27a188831d54b01f36195c2e8893d2dc9f51f
Hamish Milne <hammil@theovermare.com>
2015-03-23 18:31:37 +0000

    Project structure

---
+++ QChatLib/Enumerations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QChatServer
+{
+	public enum Request : byte
+	{
+		KeepAlive,
+		Login,
+		Logout,
+		GetIP,
+		GetNotification,
+		SendContact,
+		AcceptContact,
+		RejectContact,
+	}
+}
---
+++ QChatServer/Enumerations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QChatServer
+{
+	public enum Request : byte
+	{
+		KeepAlive,
+		Login,
+		Logout,
+		GetIP,
+		GetNotification,
+		SendContact,
+		AcceptContact,
+		RejectContact,
+	}
+
+	public enum Response : byte
+	{
+		Success,
+		Fail,
+		AuthenticationError,
+		NotPermitted,
+		InvalidArgument,
+	}
+}
